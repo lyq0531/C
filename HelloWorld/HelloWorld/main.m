a38c32dac6ccc186858aa45e95a50a0da73df175
ITPard <luyuqiang@outlook.com>
2016-05-12 14:31:23 +0800

    01-项目创建
    刚刚完成项目创建 什么都没有做

---
+++ HelloWorld/HelloWorld/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  HelloWorld
+//
+//  Created by Label on 5/12/16.
+//  Copyright © 2016 Label. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
